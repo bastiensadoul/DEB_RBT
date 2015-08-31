832cf5625626a6301311d28274a053ca0dfd6c96
bastiensadoul <bastiensadoul@hotmail.fr>
2015-08-31 10:00:37 -0600

    Raw templates
    Scripts from Bas K.

---
+++ mydata_Oncorhynchus_mykiss.m
@@ -0,0 +1,115 @@
+function [data, auxData, metaData, txtData, weights] = mydata_Oncorhynchus_mykiss
+
+%% set metadata
+metaData.phylum     = 'Chordata'; 
+metaData.class      = 'Actinopterygii'; 
+metaData.order      = 'Salmoniformes'; 
+metaData.family     = 'Salmonidae';
+metaData.species    = 'Oncorhynchus_mykiss'; 
+metaData.species_en = 'rainbow trout'; 
+metaData.T_typical  = C2K(15.5); % K, body temp
+metaData.data_0     = {'ah'; 'ab'; 'ap'; 'am'; 'Lb'; 'Lp'; 'Li'; 'Wwi'; 'Ri'};  % tags for different types of zero-variate data
+metaData.data_1     = {'L-Ww'}; % tags for different types of uni-variate data
+
+metaData.COMPLETE = 2.4; % using criteria of LikaKear2011
+
+metaData.author   = {'Bas Kooijman'};        
+metaData.date_acc = [2014 09 26];                           
+metaData.email    = {'bas.kooijman@vu.nl'};                 
+metaData.address  = {'VU University Amsterdam'}; 
+
+%% set data
+% zero-variate data
+data.ah = 33;      units.ah = 'd';    label.ah = 'age at hatch';           bibkey.ah = 'YaniHisa2002'; % 4-5 month 
+  temp.ah = C2K(8.5);  units.temp.ah = 'K'; label.temp.ah = 'temperature';
+  comment.ah = '30-36 d';
+data.ab = data.ah + 8.5; units.ab = 'd'; label.ab = 'age at birth';        bibkey.ab = 'YaniHisa2002'; 
+  temp.ab = C2K(7); units.temp.ab = 'K'; label.temp.ab = 'temperature';
+  comment.ab = 'ah + 19-21 d';
+data.ap = 2.5*365; units.ap = 'd';    label.ap = 'age at puberty';         bibkey.ap = 'fishbase';
+  temp.ap = C2K(5); units.temp.ap = 'K'; label.temp.ap = 'temperature';
+data.am = 11*365;  units.am = 'd';    label.am = 'life span';              bibkey.am = {'fishbase'};   
+  temp.am = C2K(5); units.temp.am = 'K'; label.temp.am = 'temperature';
+
+%data.L0 = 0.45;    units.L0 = 'cm';   label.L0 = 'egg diameter';           bibkey.Lb = 'Wiki'; 
+data.Lb = 2;       units.Lb = 'cm';   label.Lb = 'total length at birth';  bibkey.Lb = 'Kooy2014'; 
+data.Lp = 15;      units.Lp = 'cm';   label.Lp = 'total length at puberty';bibkey.Lp = 'fishbase';
+data.Li = 120;     units.Li = 'cm';   label.Li = 'ultimate total length';  bibkey.Li = {'fishbase'};
+
+data.Wi = 25400;   units.Wi = 'g';    label.Wi = 'ultimate wet weight';    bibkey.Wi = {'fishbase'};
+  
+data.Ri = data.Wi * 2.5/ 365; units.Ri = '#/d'; label.Ri = 'maximum reprod rate'; bibkey.Ri = {'Wiki'};   
+  temp.Ri = C2K(5); units.temp.Ri = 'K'; label.temp.Ri = 'temperature';
+  comment.Ri = '2000 till 3000 eggs per kg';
+    
+% uni-variate data
+% t-Ww data from YaniHisa2002 at T = 273 + 8.5
+% initial weight 1.54 g
+data.tW = [... % time (d), wet weight (g)
+0.202	1.471
+15.609	2.434
+31.397	3.448
+46.426	4.716
+62.217	6.136
+77.253	8.215
+92.859	10.294
+108.271	11.917
+123.688	14.148
+139.104	16.227
+154.725	20.335];
+units.tW = {'d', 'g'};  label.tW = {'time', 'wet weight'};  bibkey.tW = {'YaniHisa2002'};
+ temp.tW = C2K(8.5); units.temp.tW = 'K'; label.temp.tW = 'temperature';
+ 
+%% set weights for all real data
+weights = setweights(data, []);
+
+%% set pseudodata and respective weights
+[data, units, label, weights] = addpseudodata(data, units, label, weights);
+
+%% pack auxData and txtData for output
+auxData.temp = temp;
+txtData.units = units;
+txtData.label = label;
+txtData.bibkey = bibkey;
+txtData.comment = comment;
+
+%% Facts
+F1 = 'Many subspecies exist, e.g. O. m. irideus  (coastal rainbow trout), O. m. gairdneri (Columbia River redband trout)';
+metaData.bibkey.F1 = 'Wiki';
+F2 = 'Best culturing temp 15-16 C';
+metaData.bibkey.F2 = 'YaniHisa2002';
+F3 = 'Able to spawn several times, each time separated by months';
+metaData.bibkey.F3 = 'Wiki';
+metaData.facts = struct('F1',F1,'F2',F2,'F3',F3);
+                                 
+%% References
+bibkey = 'Wiki'; type = 'Misc'; bib = ...
+'howpublished = {\url{http://en.wikipedia.org/wiki/wiki/Oncorhynchus_mykiss}';  
+metaData.biblist.(bibkey) = ['''@', type, '{', bibkey, ', ' bib, '}'';'];
+%
+bibkey = 'Kooy2010'; type = 'Book'; bib = [ ...  % used in setting of chemical parameters and pseudodata
+'author = {Kooijman, S.A.L.M.}, ' ...
+'year = {2010}, ' ...
+'title  = {Dynamic Energy Budget theory for metabolic organisation}, ' ...
+'publisher = {Cambridge Univ. Press, Cambridge}, ' ...
+'pages = {Table 4.2 (page 150), 8.1 (page 300)}, ' ...
+'howpublished = {\url{http://www.bio.vu.nl/thb/research/bib/Kooy2010.html}'];
+metaData.biblist.(bibkey) = ['''@', type, '{', bibkey, ', ' bib, '}'';'];
+%
+bibkey = 'YaniHisa2002'; type = 'Article'; bib = [ ...  
+'author = {T. Yanik, S. A. Hisar and C. Bölükbas}, ' ...
+'year = {2002}, ' ...
+'title = {EARLY DEVELOPMENT AND GROWTH OF ARCTIC CHARR (SALVELINUS ALPINUS) AND RAINBOW TROUT (ONCORHYNCHUS MYKISS) AT A LOW WATER TEMPERATURE.}, ' ... 
+'journal = {The Israeli Journal of Aquaculture – Bamidgeh}, ' ...
+'volume = {54(2)}, '...
+'pages = {73}'];
+metaData.biblist.(bibkey) = ['''@', type, '{', bibkey, ', ' bib, '}'';'];
+%
+bibkey = 'fishbase'; type = 'Misc'; bib = ...
+'howpublished = {\url{http://www.fishbase.org/summary/239}';  
+metaData.biblist.(bibkey) = ['''@', type, '{', bibkey, ', ' bib, '}'';'];
+%
+bibkey = 'Kooy2014'; type = 'Misc'; bib = ...
+'note = {taken from from Salmo trutta}';  
+metaData.biblist.(bibkey) = ['''@', type, '{', bibkey, ', ' bib, '}'';'];
+  
