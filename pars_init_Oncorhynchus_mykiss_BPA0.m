b7ee40eb1f80a0c870fcda64da1fa3e7fff7b98b
Starrlight Augustine <starrlight.augustine@akvaplan.niva.no>
2016-02-15 18:00:31 +0100

    made change so you can estiamte

--- pars_init_Oncorhynchus_mykiss_BPA0.m
+++ pars_init_Oncorhynchus_mykiss_BPA0.m
@@ -20,18 +20,21 @@ par.E_G = 5025.245;  free.E_G = 0;  units.E_G = 'J/cm^3';  label.E_G = '[E_G], s
 par.E_Hb = 106.1359;  free.E_Hb = 0;  units.E_Hb = 'J';  label.E_Hb = 'maturity at birth'; 
 par.E_Hj = 2771.7214;  free.E_Hj = 0;  units.E_Hj = 'J';  label.E_Hj = 'maturity at metam'; 
 par.E_Hp = 5796157.9753;  free.E_Hp = 0;  units.E_Hp = 'J';  label.E_Hp = 'maturity at puberty'; 
+par.h_a = 2.1754e-50;free.h_a   = 0; units.h_a = '1/d^2';    label.h_a = 'Weibull aging acceleration';
+par.s_G = 10;       free.s_G   = 0; units.s_G = '-';        label.s_G = 'Gompertz stress coefficient';
 
 %% other parameters 
 par.T_A = 6237.8303;  free.T_A = 0;  units.T_A = 'K';  label.T_A = 'Arrhenius temp'; 
 par.T_ref = 293.15;  free.T_ref = 0;  units.T_ref = 'K';  label.T_ref = 'Reference temperature'; 
-par.del_M = 0.11347;  free.del_M = 0;  units.del_M = '-';  label.del_M = 'shape coefficient DaviKlemm2014'; 
-par.f_tW_gw124fin = 0.73906;  free.f_tW_gw124fin = 0;  units.f_tW_gw124fin = '-';  label.f_tW_gw124fin = 'scaled functional response for gw124fin'; 
-par.f_tW_gw124iniA = 0.65928;  free.f_tW_gw124iniA = 0;  units.f_tW_gw124iniA = '-';  label.f_tW_gw124iniA = 'scaled functional response for gw124ini tank A'; 
-par.f_tW_gw124iniB = 0.61494;  free.f_tW_gw124iniB = 0;  units.f_tW_gw124iniB = '-';  label.f_tW_gw124iniB = 'scaled functional response for gw124ini tank B'; 
-par.f_tW_gw124iniC = 0.65547;  free.f_tW_gw124iniC = 0;  units.f_tW_gw124iniC = '-';  label.f_tW_gw124iniC = 'scaled functional response for gw124ini tank C'; 
-par.f_tW_gw150A = 0.54634;  free.f_tW_gw150A = 0;  units.f_tW_gw150A = '-';  label.f_tW_gw150A = 'scaled functional response for gw150 tank A'; 
-par.f_tW_gw150B = 0.55039;  free.f_tW_gw150B = 0;  units.f_tW_gw150B = '-';  label.f_tW_gw150B = 'scaled functional response for gw150 tank B'; 
-par.f_tW_gw150C = 0.55059;  free.f_tW_gw150C = 1;  units.f_tW_gw150C = '-';  label.f_tW_gw150C = 'scaled functional response for gw150 tank C'; 
+% par.del_M = 0.11347;  free.del_M = 0;  units.del_M = '-';  label.del_M = 'shape coefficient DaviKlemm2014'; 
+par.f = 1; free.f  = 0; units.f = '-';       label.f = 'scaled functional response just for check_my_pet?????';
+par.f_tW_gw124fin = 1;  free.f_tW_gw124fin = 1;  units.f_tW_gw124fin = '-';  label.f_tW_gw124fin = 'scaled functional response for gw124fin'; 
+par.f_tW_gw124iniA = 1;  free.f_tW_gw124iniA = 1;  units.f_tW_gw124iniA = '-';  label.f_tW_gw124iniA = 'scaled functional response for gw124ini tank A'; 
+par.f_tW_gw124iniB = 1;  free.f_tW_gw124iniB = 1;  units.f_tW_gw124iniB = '-';  label.f_tW_gw124iniB = 'scaled functional response for gw124ini tank B'; 
+par.f_tW_gw124iniC = 1;  free.f_tW_gw124iniC = 1;  units.f_tW_gw124iniC = '-';  label.f_tW_gw124iniC = 'scaled functional response for gw124ini tank C'; 
+par.f_tW_gw150A = 1;  free.f_tW_gw150A = 1;  units.f_tW_gw150A = '-';  label.f_tW_gw150A = 'scaled functional response for gw150 tank A'; 
+par.f_tW_gw150B = 1;  free.f_tW_gw150B = 1;  units.f_tW_gw150B = '-';  label.f_tW_gw150B = 'scaled functional response for gw150 tank B'; 
+par.f_tW_gw150C = 1;  free.f_tW_gw150C = 1;  units.f_tW_gw150C = '-';  label.f_tW_gw150C = 'scaled functional response for gw150 tank C'; 
 
 %% set chemical parameters from Kooy2010 
 [par, units, label, free] = addchem(par, units, label, free, metaData.phylum, metaData.class);
