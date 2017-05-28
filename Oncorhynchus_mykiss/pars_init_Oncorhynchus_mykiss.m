dfb459ddc96b4685167e881fb4e5aa789bf6a8d0
Starrlight Augustine <starrlight.augustine@akvaplan.niva.no>
2017-05-28 09:43:04 +0200

    corrected bibs in the mydata file

--- Oncorhynchus_mykiss/pars_init_Oncorhynchus_mykiss.m
+++ Oncorhynchus_mykiss/pars_init_Oncorhynchus_mykiss.m
@@ -6,32 +6,32 @@ metaPar.model = 'abj';
 par.T_ref = 293.15;   free.T_ref = 0;   units.T_ref = 'K';        label.T_ref = 'Reference temperature'; 
 
 %% core primary parameters 
-par.z = 6.2761;       free.z     = 1;   units.z = '-';            label.z = 'zoom factor'; 
+par.z = 5.1106;       free.z     = 0;   units.z = '-';            label.z = 'zoom factor'; 
 par.F_m = 6.5;        free.F_m   = 0;   units.F_m = 'l/d.cm^2';   label.F_m = '{F_m}, max spec searching rate'; 
 par.kap_X = 0.8;      free.kap_X = 0;   units.kap_X = '-';        label.kap_X = 'digestion efficiency of food to reserve'; 
 par.kap_P = 0.1;      free.kap_P = 0;   units.kap_P = '-';        label.kap_P = 'faecation efficiency of food to faeces'; 
-par.v = 0.052553;     free.v     = 1;   units.v = 'cm/d';         label.v = 'energy conductance'; 
-par.kap = 0.8;        free.kap   = 1;   units.kap = '-';          label.kap = 'allocation fraction to soma'; 
+par.v = 0.048601;     free.v     = 0;   units.v = 'cm/d';         label.v = 'energy conductance'; 
+par.kap = 0.55991;    free.kap   = 0;   units.kap = '-';          label.kap = 'allocation fraction to soma'; 
 par.kap_R = 0.95;     free.kap_R = 0;   units.kap_R = '-';        label.kap_R = 'reproduction efficiency'; 
-par.p_M = 221.7035;   free.p_M   = 1;   units.p_M = 'J/d.cm^3';   label.p_M = '[p_M], vol-spec somatic maint'; 
+par.p_M = 370.4783;   free.p_M   = 0;   units.p_M = 'J/d.cm^3';   label.p_M = '[p_M], vol-spec somatic maint'; 
 par.p_T = 0;          free.p_T   = 0;   units.p_T = 'J/d.cm^2';   label.p_T = '{p_T}, surf-spec somatic maint'; 
 par.k_J = 0.002;      free.k_J   = 0;   units.k_J = '1/d';        label.k_J = 'maturity maint rate coefficient'; 
-par.E_G = 5025;       free.E_G   = 1;   units.E_G = 'J/cm^3';     label.E_G = '[E_G], spec cost for structure'; 
-par.E_Hb = 3.126e+01; free.E_Hb  = 1;   units.E_Hb = 'J';         label.E_Hb = 'maturity at birth'; 
-par.E_Hj = 5.479e+02; free.E_Hj  = 1;   units.E_Hj = 'J';         label.E_Hj = 'maturity at metam'; 
-par.E_Hp = 2.102e+06; free.E_Hp  = 1;   units.E_Hp = 'J';         label.E_Hp = 'maturity at puberty'; 
-par.h_a = 3.288e-28;  free.h_a   = 1;   units.h_a = '1/d^2';      label.h_a = 'Weibull aging acceleration'; 
+par.E_G = 5237.6666;  free.E_G   = 0;   units.E_G = 'J/cm^3';     label.E_G = '[E_G], spec cost for structure'; 
+par.E_Hb = 5.737e+01; free.E_Hb  = 0;   units.E_Hb = 'J';         label.E_Hb = 'maturity at birth'; 
+par.E_Hj = 7.701e+02; free.E_Hj  = 0;   units.E_Hj = 'J';         label.E_Hj = 'maturity at metam'; 
+par.E_Hp = 4.879e+06; free.E_Hp  = 0;   units.E_Hp = 'J';         label.E_Hp = 'maturity at puberty'; 
+par.h_a = 3.004e-24;  free.h_a   = 0;   units.h_a = '1/d^2';      label.h_a = 'Weibull aging acceleration'; 
 par.s_G = 10;         free.s_G   = 0;   units.s_G = '-';          label.s_G = 'Gompertz stress coefficient'; 
 
 %% other parameters 
-par.E_Hh = 1.144e+01; free.E_Hh  = 1;   units.E_Hh = 'J';         label.E_Hh = 'maturity at hatch'; 
-par.T_A = 9635.6551;  free.T_A   = 1;   units.T_A = 'K';          label.T_A = 'Arrhenius temp'; 
-par.del_M = 0.13022;  free.del_M = 1;   units.del_M = '-';        label.del_M = 'shape coef (forked length)'; 
-par.del_M2 = 0.11005;  free.del_M2 = 1;   units.del_M2 = '-';       label.del_M2 = 'shape coef, LW  '; 
+par.E_Hh = 1.650e+01; free.E_Hh  = 0;   units.E_Hh = 'J';         label.E_Hh = 'maturity at hatch'; 
+par.T_A = 10923.9752;  free.T_A   = 0;   units.T_A = 'K';          label.T_A = 'Arrhenius temp'; 
+par.del_M = 0.10137;  free.del_M = 0;   units.del_M = '-';        label.del_M = 'shape coef (forked length)'; 
+par.del_M2 = 0.087621;  free.del_M2 = 0;   units.del_M2 = '-';       label.del_M2 = 'shape coef, LW  '; 
 par.f = 1;            free.f     = 0;   units.f = '-';            label.f = 'scaled functional response for 0-var data'; 
 par.f_tW1 = 1;        free.f_tW1 = 0;   units.f_tW1 = '-';        label.f_tW1 = 'scld. fctl. res.  DaviKenn2014 (first part of experiment)'; 
 par.f_tW2 = 1;        free.f_tW2 = 0;   units.f_tW2 = '-';        label.f_tW2 = 'scld. fctl. res.  DaviKenn2014 (second part of experiment)'; 
-par.f_tW3 = 0.42391;  free.f_tW3 = 1;   units.f_tW3 = '-';        label.f_tW3 = 'scld. fctl. res. YaniHisa2002'; 
+par.f_tW3 = 0.4597;   free.f_tW3 = 0;   units.f_tW3 = '-';        label.f_tW3 = 'scld. fctl. res. YaniHisa2002'; 
 
 %% set chemical parameters from Kooy2010 
 [par, units, label, free] = addchem(par, units, label, free, metaData.phylum, metaData.class);
--- Oncorhynchus_mykiss/run_Oncorhynchus_mykiss.m
+++ Oncorhynchus_mykiss/run_Oncorhynchus_mykiss.m
@@ -1,4 +1,4 @@
-% clear all; close all; clc
+close all; 
 
 global pets
 
@@ -8,8 +8,8 @@ estim_options('default'); % runs estimation, uses nmregr method and filter
 estim_options('max_step_number',5e3);  % set options for parameter estimation
 estim_options('max_fun_evals',5e4);    % set options for parameter estimation
 
-estim_options('pars_init_method', 1);  % 1 from mat files (continuation)
-estim_options('results_output', 1);    %
-estim_options('method', 'nm');
+estim_options('pars_init_method',2);  % 1 from mat files (continuation)
+estim_options('results_output', 0);    %
+estim_options('method', 'no');
 estim_pars;          % run estimation
 
