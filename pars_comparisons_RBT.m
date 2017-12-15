23597c775fe50c398eef822dbcb4b3dc61155179
Starrlight Augustine <starrlight.augustine@akvaplan.niva.no>
2017-12-15 15:07:32 +0100

    pars_comparisons
    add graphs Li_Ri et Li_Wd0

--- pars_comparisons_RBT.m
+++ pars_comparisons_RBT.m
@@ -1,41 +1,41 @@
-% close all;  
-% clear all;  clc
-% 
-% % Calls AmPtool - pars has parameter values in columns and each species in
-% % lines
-% pars = read_allStat('p_Am','v', 'E_Hb', 'E_Hj', 'E_Hp','s_M', 'L_i','p_M',  'kap', 'E_m', ...
-%     'h_a', 'a_m', 'L_m', 'T_A', 'c_T', 'J_Oi', 'Wd_i', 'R_i', 'N_i', 'Wd_0');
-% 
-% % unpack parameters & other statistics
-% % keep in mind that parameters are at T_ref 
-% % statistics (with time in their dimension) are at T_typical
-% p_Am = pars(:,1);
-% v    = pars(:,2);
-% E_Hb = pars(:,3);
-% E_Hj = pars(:,4);
-% E_Hp = pars(:,5);
-% s_M  = pars(:,6);
-% L_i  = pars(:,7);
-% p_M  = pars(:,8);
-% kap  = pars(:,9);
-% E_m  = pars(:,10);
-% h_a  = pars(:,11);
-% a_m  = pars(:,12);
-% L_m  = pars(:,13);
-% T_A = pars(:,14);
-% c_T = pars(:,15);
-% 
-% J_Oi = pars(:,16); JT_Oi = J_Oi./ c_T; % correct to reference temp
-% Wd_i = pars(:,17);
-% R_i = pars(:,18);
-% N_i = pars(:,19);
-% Wd_0 = pars(:,20);
-% 
-% [selM, srcM] = select_01('Animalia','Actinopterygii'); % indices for Bony fish
-% [selB, srcB] = select_01('Animalia','Salmoniformes'); % indices for salmoniformes
-% % [selA, srcA] = select_01('Animalia','Salmoniformes');  % indices for Arctica Islandica
-% [selT, srcT] = select_01('Animalia','Oncorhynchus_mykiss'); % indices for Tridacna gigas
-% 
+close all;  
+clear all;  clc
+
+% Calls AmPtool - pars has parameter values in columns and each species in
+% lines
+pars = read_allStat('p_Am','v', 'E_Hb', 'E_Hj', 'E_Hp','s_M', 'L_i','p_M',  'kap', 'E_m', ...
+    'h_a', 'a_m', 'L_m', 'T_A', 'c_T', 'J_Oi', 'Wd_i', 'R_i', 'N_i', 'Wd_0');
+
+% unpack parameters & other statistics
+% keep in mind that parameters are at T_ref 
+% statistics (with time in their dimension) are at T_typical
+p_Am = pars(:,1);
+v    = pars(:,2);
+E_Hb = pars(:,3);
+E_Hj = pars(:,4);
+E_Hp = pars(:,5);
+s_M  = pars(:,6);
+L_i  = pars(:,7);
+p_M  = pars(:,8);
+kap  = pars(:,9);
+E_m  = pars(:,10);
+h_a  = pars(:,11);
+a_m  = pars(:,12);
+L_m  = pars(:,13);
+T_A = pars(:,14);
+c_T = pars(:,15);
+
+J_Oi = pars(:,16); JT_Oi = J_Oi./ c_T; % correct to reference temp
+Wd_i = pars(:,17);
+R_i = pars(:,18)./ c_T; % correct to reference temp
+N_i = pars(:,19);
+Wd_0 = pars(:,20);
+
+[selM, srcM] = select_01('Animalia','Actinopterygii'); % indices for Bony fish
+[selB, srcB] = select_01('Animalia','Salmoniformes'); % indices for salmoniformes
+% [selA, srcA] = select_01('Animalia','Salmoniformes');  % indices for Arctica Islandica
+[selT, srcT] = select_01('Animalia','Oncorhynchus_mykiss'); % indices for Tridacna gigas
+
 
 %% Max respiration (at T_ref) as function of Wd_i (ultimate dry weight) 
 
@@ -186,12 +186,38 @@ plot(log10(L_i(selM)), log10(E_m(selM)), 'k.', 'markersize', 8)
 plot(log10(L_i(selB)), log10(E_m(selB)), 'bo', 'markersize',6,'markerfacecolor','b')
 % plot(log10(L_i(selA)), log10(E_m(selA)), 'gs', 'markersize',12,'markerfacecolor','g')
 plot(log10(L_i(selT)), log10(E_m(selT)), 'bs', 'markersize',12,'markerfacecolor','b')
-plot(log10(L_i(selM)), log10(22.5 * L_i(selM)/ 0.02), 'color',[0.7 0.7 0.7], 'linewidth',1, 'linestyle', '--')
+% plot(log10(L_i(selM)), log10(22.5 * L_i(selM)/ 0.02), 'color',[0.7 0.7 0.7], 'linewidth',1, 'linestyle', '--')
 xlabel('Ultimate struct. length, _{10}log L_i, cm')
 ylabel('Max. res. dens. _{10}log [E_m], J/cm^3')
 xlim([min(log10(L_i(selM))) max(log10(L_i(selM)))]  )
 set(gca, 'FontSize', 15, 'Box', 'on')
 saveas(gca,'figs/Li_Em.png')
+
+%% R_i as function of L_inf
+figure(); hold on
+plot(log10(L_i(selM)), log10(R_i(selM)), 'k.', 'markersize', 8)
+plot(log10(L_i(selB)), log10(R_i(selB)), 'bo', 'markersize',6,'markerfacecolor','b')
+% plot(log10(L_i(selA)), log10(E_m(selA)), 'gs', 'markersize',12,'markerfacecolor','g')
+plot(log10(L_i(selT)), log10(R_i(selT)), 'bs', 'markersize',12,'markerfacecolor','b')
+% plot(log10(L_i(selM)), log10(22.5 * L_i(selM)/ 0.02), 'color',[0.7 0.7 0.7], 'linewidth',1, 'linestyle', '--')
+xlabel('Ultimate struct. length, _{10}log L_i, cm')
+ylabel('Max. reprod. rate. _{10}log [R_i], 1/d')
+xlim([min(log10(L_i(selM))) max(log10(L_i(selM)))]  )
+set(gca, 'FontSize', 15, 'Box', 'on')
+saveas(gca,'figs/Li_Ri.png')
+
+%% Wd_0 as function of L_inf
+figure(); hold on
+plot(log10(L_i(selM)), log10(Wd_0(selM)), 'k.', 'markersize', 8)
+plot(log10(L_i(selB)), log10(Wd_0(selB)), 'bo', 'markersize',6,'markerfacecolor','b')
+% plot(log10(L_i(selA)), log10(E_m(selA)), 'gs', 'markersize',12,'markerfacecolor','g')
+plot(log10(L_i(selT)), log10(Wd_0(selT)), 'bs', 'markersize',12,'markerfacecolor','b')
+% plot(log10(L_i(selM)), log10(22.5 * L_i(selM)/ 0.02), 'color',[0.7 0.7 0.7], 'linewidth',1, 'linestyle', '--')
+xlabel('Ultimate struct. length, _{10}log L_i, cm')
+ylabel('Egg dry weight _{10}log [Wd_0], g')
+xlim([min(log10(L_i(selM))) max(log10(L_i(selM)))]  )
+set(gca, 'FontSize', 15, 'Box', 'on')
+saveas(gca,'figs/Li_Wd0.png')
 
 
 %%
